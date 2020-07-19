a3c6427694a514ed4ef7265d0266d729c9f5b367
Antonino <antonino29@outlook.com>
2020-07-19 22:11:11 +0200

    optimize tmux-cmus scripts

--- Build-Folder/Scripts/tmuxcmus3.sh
+++ Build-Folder/Scripts/tmuxcmus3.sh
@@ -11,7 +11,13 @@ album=$( cmus-remote -Q | grep tag\ album\ | sed 's/tag album //' )
 
 artist=$( cmus-remote -Q | grep tag\ artist\ | sed 's/tag artist //' )
 
+lb=${#album}
+(( lb > 28)) &&
+        album=${album::$(( 28 ))}…
 
+lt=${#title}
+(( lt > 29)) &&
+        title=${title::$(( 30 ))}…
 
 case $status in
         playing)
