c088d197bb44962efb06f71321d01f03a7d15baf
Antonino <antonino29@outlook.com>
2020-10-28 15:01:32 +0100

    scripts update

--- Build-Folder/Scripts/decrease-padding2.sh
+++ Build-Folder/Scripts/decrease-padding2.sh
@@ -2,7 +2,7 @@
 
 old=`herbstclient get frame_padding`
 
-new=`expr $old - 10`
+new=`expr $old - 6`
 
 herbstclient set frame_padding $new
 
--- Build-Folder/Scripts/increase-padding2.sh
+++ Build-Folder/Scripts/increase-padding2.sh
@@ -2,6 +2,6 @@
 
 old=`herbstclient get frame_padding`
 
-new=`expr $old + 10`
+new=`expr $old + 6`
 
 herbstclient set frame_padding $new
