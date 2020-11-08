19f7c5aab8da5db61f44ff6c3c84eacec5c7e081
Antonino <antonino29@outlook.com>
2020-11-08 21:54:46 +0100

    little changes 2

--- Build-Folder/Scripts/wal.sh
+++ Build-Folder/Scripts/wal.sh
@@ -6,7 +6,8 @@ file=$(grep 'file=' /home/antonino/.config/nitrogen/bg-saved.cfg | sed s/file=//
 
 wal --backend colorz  -i $file -s -t -n -e
 
-color=$(grep '*color10:  ' /home/antonino/.cache/wal/colors.Xresources | sed s/"*color10:  "//)
+#color=$(grep '*color10:  ' /home/antonino/.cache/wal/colors.Xresources | sed s/"*color10:  "//)
+color=$(grep '*color13:  ' /home/antonino/.cache/wal/colors.Xresources | sed s/"*color13:  "//)
 
 sed -i -e "s/shade1 = .*/shade1 = $color/g" $HOME/.config/polybar/config
 
