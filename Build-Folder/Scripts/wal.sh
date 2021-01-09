b6933afee35822cfcde02c2bd2ea6540eb3a69cc
Antonino <antonino29@outlook.com>
2021-01-09 22:18:49 +0100

    thingy

--- Build-Folder/Scripts/wal.sh
+++ Build-Folder/Scripts/wal.sh
@@ -11,4 +11,11 @@ color=$(grep '*color13:  ' /home/antonino/.cache/wal/colors.Xresources | sed s/"
 
 sed -i -e "s/shade1 = .*/shade1 = $color/g" $HOME/.config/polybar/config
 
+sed -i -e "s/.border_color = .*/.border_color = \"$color\",/g" $HOME/Build-Folder/xmenu/config.h
+cd $HOME/Build-Folder/xmenu/ && make install
+
+herbstclient attr theme.floating.active.color $color
+sed -i -e "s/hc attr theme.active.color .*/hc attr theme.active.color \'$color\'/g" $HOME/.config/herbstluftwm/autostart
+
+
 sleep 1; herbstclient pad 0 "29" "0" "0"
