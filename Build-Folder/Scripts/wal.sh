acd54a471570029fc225c11b0991e8ca3fe9f0bc
Antonino <antonino29@outlook.com>
2021-03-13 18:29:26 +0100

    wal script updated

--- Build-Folder/Scripts/wal.sh
+++ Build-Folder/Scripts/wal.sh
@@ -15,26 +15,26 @@ color1=$(grep '*color13:  ' $HOME/.cache/wal/colors.Xresources | sed s/"*color13
 color2=$(grep '*color15:  ' $HOME/.cache/wal/colors.Xresources | sed s/"*color15:  "//)
 color3=$(grep '*color0:  ' $HOME/.cache/wal/colors.Xresources | sed s/"*color0:  "//)
 
-sed -i -e "s/#define color1 #.*/#define color1 $color1/g" $HOME/.config/glava/bars.glsl
-sed -i -e "s/#define color2 #.*/#define color2 $color2/g" $HOME/.config/glava/bars.glsl
+sed -i -e "s/#define color1 #.*/#define color1 $color1/g; \
+	s/#define color2 #.*/#define color2 $color2/g" \
+	$HOME/.config/glava/bars.glsl
 
-pgrep -x glava && pkill glava && glava --desktop &
+sed -i -e "s/ gradient_color_1 = '#.*'/ gradient_color_1 = '$color1'/g; \
+	s/ gradient_color_2 = '#.*'/ gradient_color_2 = '$color2'/g" \
+	$HOME/.config/cava/config
 
 sed -i -e "s/shade1 = .*/shade1 = $color1/g" \
 	$HOME/.config/polybar/config
 
-sed -i -e "s/\.border_color = .*/.border_color = \"$color1\",/g" \
+sed -i -e "s/\.border_color = .*/.border_color = \"$color1\",/g; \
+	s/\.background_color = .*/.background_color = \"$color3\",/g " \
 	$HOME/Build-Folder/xmenu/config.h
 
-sed -i -e "s/\.background_color = .*/.background_color = \"$color3\",/g" \
-	$HOME/Build-Folder/xmenu/config.h
-
-herbstclient attr theme.floating.active.color $color1
 sed -i -e "s/hc attr theme.active.color1 .*/hc attr theme.active.color \'$color1\'/g" \
 	$HOME/.config/herbstluftwm/autostart
 
-#pkill picom ; picom --vsync --config .config/picom/picom.conf &
-#sleep 1; herbstclient pad 0 "29" "0" "0"
-#polybar-msg cmd restart
+herbstclient attr theme.floating.active.color $color1
+pgrep -x cava && pkill -USR1 cava
+pgrep -x glava && pkill -USR1 glava
 make install -C $HOME/Build-Folder/xmenu/
 pywalfox update > /dev/null 2>&1
