84286e4d8f2b6413e733d4d8f616431de77e3e37
Antonino <antonino29@outlook.com>
2021-04-15 18:52:15 +0200

    generic update

--- Build-Folder/Scripts/play.sh
+++ Build-Folder/Scripts/play.sh
@@ -7,3 +7,5 @@
                 forw) playerctl --player=cmus,tauon,playerctld position 10+ ;;
                 back) playerctl --player=cmus,tauon,playerctld position 10- ;;
         esac
+
+	tmux refresh-client
